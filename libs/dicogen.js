ab33e0249d963b2881f976c4943ccd0832411934
tibOin <guillaumecharua@boin.noip.me>
2016-05-15 01:38:28 +0200

    First version
    Here is the first (bêta) version of the generator

--- libs/dicogen.js
+++ libs/dicogen.js
@@ -1,8 +1,4 @@
-var express = require('express');
-var router = express.Router();
-
-
-function connect_to_db(callback) {
+function connect_to_db() {
   // First you need to create a connection to the db
   var con = mysql.createConnection({
     host: "130.211.110.8",
@@ -19,9 +15,8 @@ function connect_to_db(callback) {
     console.log('Connection established');
   });
 
-  callback(con);
+  return con;
 }
-
 function close_db(db) {
   db.end(function(err) {
 
@@ -58,19 +53,19 @@ function generate(avancement, db) {
 
 }
 
-function get_avancement(db, callback) {
+function get_avancement(db) {
 
-  //var avancement = 0;
+  var avancement = 1;
 
-  db.query("SELECT COUNT(*) FROM INFORMATION_SCHEMA.Tables WHERE TABLE_NAME REGEXP '([[:digit:]]_char)+'", function(err, rows) {
+  db.query("SELECT Count(*) FROM INFORMATION_SCHEMA.Tables WHERE TABLE_NAME REGEXP '([[:digit:]]_char)+'", function(err, rows) {
     if(err) throw err;
 
-    var rawPacket = rows[0];
-    for(valueIndex in rawPacket) {
-      avancement = rawPacket[valueIndex];
-    }
-    callback(avancement);
+    avancement = rows.Count;
+
+    console.log(avancement);
   });
+
+  return avancement;
 }
 
 function create_table(avancement, db) {
@@ -94,15 +89,13 @@ function table_exist(avancement, db) {
     else {
       return true;
     }
-  });
+  })
 }
 
 function launch_generation(db, avancement, char_length) {
-  console.log('Launching generation');
-  
+
   while (avancement < char_length) {
     if (table_exist(avancement, db)) {
-      console.log('Table already exists...');
       break;
     }
     else {
@@ -113,28 +106,3 @@ function launch_generation(db, avancement, char_length) {
   }
 
 }
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { generating: false });
-});
-
-router.post('/generate', function(req, res) {
-  var char_length = req.body.user_char_length;
-  var mysql = require("mysql");
-
-  connect_to_db(function(db) {
-    get_avancement(db, function(avancement) {
-      connect_to_db(function(db2) {
-        console.log('Avancement de départ : ' + avancement);
-        launch_generation(db2, avancement, char_length);
-        close_db(db2);
-      });
-    });
-    close_db(db);
-  });
-
-  res.render('index', {generating: true});
-});
-
-module.exports = router;
